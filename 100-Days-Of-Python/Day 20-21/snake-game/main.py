63001ad8c098b89dee52361f0936a35b7161a066
Anamika Singh <anamikasingh361996@gmail.com>
2025-07-11 13:54:12 +0530

    Creation of snake body is done

--- 100-Days-Of-Python/Day 20-21/snake-game/main.py
+++ 100-Days-Of-Python/Day 20-21/snake-game/main.py
@@ -1,5 +1,20 @@
+# 1. Create a snake body
+# 2. Move the snake
+# 3. Control the snack
 # 4. Detect collision with food
 # 5. create a scoreboard
 # 6. Detect collision with wall
 # 7. Detect collision with tail
 
+from turtle import Screen, Turtle
+
+snake = Turtle("square") # can provide the shape while creating an object
+snake.shapesize(1, 3)
+snake.color("white")
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.exitonclick()
