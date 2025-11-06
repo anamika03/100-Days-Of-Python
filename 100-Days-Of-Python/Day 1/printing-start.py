c3b2fc7d7120f399741186e04f95ed9bbff4e928
Anamika Singh <anamikasingh361996@gmail.com>
2025-11-06 12:28:33 +0100

    updated the codes

--- 100-Days-Of-Python/Day 1/printing-start.py
+++ 100-Days-Of-Python/Day 1/printing-start.py
@@ -1,1 +1,17 @@
-print("Something")
+print("\nSomething")
+print("Hello World!")
+print("Welcome to Python programming.")
+print("Let's start coding!")
+print("This is the first day of learning Python.")
+print("Enjoy your coding journey!")
+print("Happy" + " coding!")
+name = input("What is your name? ")
+print("Hello " + name + "! Welcome to the world of Python.")
+# cmd + / = comment shortcut
+
+#Python Variables
+my_name = "Rashika"   #string variable
+my_age = 19   #integer variable
+print("My name is " + my_name)
+print("I am " + str(my_age) + " years old.")
+print(len(my_name))  #len() function to get length of string
