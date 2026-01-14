6058233965584993237a7f08ba2909e91e79dcec
anamika03 <anamikasingh361996@gmail.com>
2026-01-14 12:59:57 +0100

    code added

---
+++ 100-Days-Of-Python/Day 25/US_States_Game/main.py
@@ -0,0 +1,45 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("US States Game")
+screen.setup(width=800, height=600)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# Function to get mouse click coordinates
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+
+import pandas
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state].iloc[0]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+
+# screen.exitonclick()
+
+
