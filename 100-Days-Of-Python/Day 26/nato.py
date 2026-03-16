562ddec8250f71be48f97e51bbf67dbec20c5581
anamika03 <anamikasingh361996@gmail.com>
2026-03-16 21:37:25 +0530

    changed done in nato.py file: implemented error handling

--- 100-Days-Of-Python/Day 26/nato.py
+++ 100-Days-Of-Python/Day 26/nato.py
@@ -3,16 +3,24 @@
 import pandas
 import csv
 
-# TODO: Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(nato_dict)
 
 
-# TODO: Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a Name: ").upper()
-# print(user_input)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    user_input = input("Enter a Name: ").upper()
+    try:
+        nato_code = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Please enter a valid name containing only letters.")
+        generate_phonetic()
+    else:
+        print(nato_code)
+        print("\n")
 
-nato_code = [nato_dict[letter] for letter in user_input]
-print(nato_code)
+generate_phonetic()
+
---
+++ 100-Days-Of-Python/Day 30/ValueError.py
@@ -0,0 +1,24 @@
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
+# --------------------------------------------------------------------
+fruits = ["Apple", "Pear", "Orange"]
+
+# Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+make_pie(4)
+
+
