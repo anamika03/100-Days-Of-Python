709f2018dcbd942c89bfbf3eecb5820e6e7e94ee
anamika03 <anamikasingh361996@gmail.com>
2026-03-16 22:47:33 +0530

    Day 29 filed edited and added new SEARCH feature in it

--- 100-Days-Of-Python/Day 29/main.py
+++ 100-Days-Of-Python/Day 29/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, shuffle, randint
+import json
 
 WHITE = "#fefefe"
 YELLOW = "#efefea"
@@ -23,26 +24,60 @@ def generate_password():
     shuffle(password_list)
 
     password = "".join(password_list)
+    Password_entry.delete(0, END)
     Password_entry.insert(0, password)
-
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
 
     website = Website_entry.get()
     email = Email_entry.get()
     password = Password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \n\nIs it ok to save?\n")   
-
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                Website_entry.delete(0, END)
-                Email_entry.delete(0, END)
-                Password_entry.delete(0, END)
+        try:       
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+            
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            # data_file.write(f"{website} | {email} | {password}\n")
+            Website_entry.delete(0, END)
+            # Email_entry.delete(0, END)
+            Password_entry.delete(0, END)
+            
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = Website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -65,8 +100,8 @@ Password = Label(text="Password:", bg=WHITE, fg=BLACK)
 Password.grid(column=0, row=3)
 
 #Entries
-Website_entry = Entry(width=39, bg=WHITE, fg=BLACK, highlightthickness=0, insertbackground=BLUE)
-Website_entry.grid(column=1, row=1, columnspan=2)
+Website_entry = Entry(width=22, bg=WHITE, fg=BLACK, highlightthickness=0, insertbackground=BLUE)
+Website_entry.grid(column=1, row=1)
 Website_entry.focus()
 
 Email_entry = Entry(width=39, bg=WHITE, fg=BLACK, highlightthickness=0, insertbackground=BLUE)
@@ -81,5 +116,7 @@ generate_password_button = Button(text="Generate Password", fg=BLACK, highlightt
 generate_password_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=36, fg=BLACK, highlightthickness=0, highlightbackground=WHITE, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+search_button = Button(text="Search", width=13, fg=BLACK, highlightthickness=0, highlightbackground=WHITE, command=find_password)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
