3d542d8b544a62d185c909547e46e47c9b707afe
anamika03 <anamikasingh361996@gmail.com>
2026-04-09 02:03:37 +0200

    edited in day 32

--- 100-Days-Of-Python/Day 32/smtp_time_module.py
+++ 100-Days-Of-Python/Day 32/smtp_time_module.py
@@ -1,29 +1,36 @@
-# import smtplib
+from random import random
+import smtplib
 
-# my_email = "singhanamika5647@gmail.com" #for yahoo use "smtp.mail.yahoo.com"
-# password = "fyyxitktgkzetqer"
+my_email = "singhanamika5647@gmail.com" #for yahoo use "smtp.mail.yahoo.com"
+password = "owpywxwwvwqugzyf"
+with open("/Users/anamika/Documents/100_Days_of_Python/100-Days-Of-Python/Day 32/quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        
+    print(quote)
 
-# with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-#     connection.starttls() 
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email, 
-#         to_addrs="rashikasingh191@gmail.com", 
-#         msg="Subject:Test\n\nThis is the another dummy email."
-#     )
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls() 
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+        from_addr=MY_EMAIL, 
+        to_addrs=MY_EMAIL, 
+        msg=f"Subject:Monday Motivation\n\n{quote}"
+    )
+print("Email sent successfully!")
 
-import datetime as dt
+# import datetime as dt
 
-now = dt.datetime.now()
-print(now)
-day_of_week = now.weekday()
-print(day_of_week)  #0 is Monday and 6 is Sunday
-# print(now.year)   #data type is 'int'
-# print(now.month)
-# print(now.day)
+# now = dt.datetime.now()
+# print(now)
+# day_of_week = now.weekday()
+# print(day_of_week)  #0 is Monday and 6 is Sunday
+# # print(now.year)   #data type is 'int'
+# # print(now.month)
+# # print(now.day)
 
-date_of_birth = dt.datetime(year=1996, month=6, day=3)
-print(date_of_birth)
+# date_of_birth = dt.datetime(year=1996, month=6, day=3)
+# print(date_of_birth)
 
 
 
