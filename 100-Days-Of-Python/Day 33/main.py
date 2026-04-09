083ecfcee732412894c7e9457ed6e4548fe59146
anamika03 <anamikasingh361996@gmail.com>
2026-04-09 11:43:52 +0200

    tried to get the api response

--- 100-Days-Of-Python/Day 33/main.py
+++ 100-Days-Of-Python/Day 33/main.py
@@ -15,20 +15,30 @@
 
 import requests
 
+# response = requests.get("http://api.open-notify.org/iss-now.json")
+# print(response) # This will print the response object, which contains information about the HTTP response received from the server. 
+# #It includes details such as the status code, headers, etc. The status code indicates whether the request was successful 
+# # (e.g., 200 for OK) or if there was an error (e.g., 404 for Not Found). 
+
+# # 1XX: Hold open
+# # 2XX: Success
+# # 3XX: GO Away/Redirection
+# # 4XX: You Screwed Up/Client Error
+# # 5XX: I screwed Up/Server Error
+
+# print(response.status_code) # This will print the status code of the HTTP response, which indicates the result of the request.
+
+# if response.status_code != 200:
+#     raise Exception("Error: API request unsuccessful") # This line raises an exception if the status code is not 200, indicating that the API request was unsuccessful.
+
+# response.raise_for_status() # This line will raise an HTTPError if the HTTP request returned an unsuccessful status code (i.e., not in the 200-299 range).
+
 response = requests.get("http://api.open-notify.org/iss-now.json")
-print(response) # This will print the response object, which contains information about the HTTP response received from the server. 
-#It includes details such as the status code, headers, etc. The status code indicates whether the request was successful 
-# (e.g., 200 for OK) or if there was an error (e.g., 404 for Not Found). 
+response.raise_for_status()
 
-# 1XX: Hold open
-# 2XX: Success
-# 3XX: GO Away/Redirection
-# 4XX: You Screwed Up/Client Error
-# 5XX: I screwed Up/Server Error
+latitude = response.json()["iss_position"]["latitude"] # This line extracts the latitude of the International Space Station (ISS) from the JSON response received from the API.
+longitude = response.json()["iss_position"]["longitude"] # This line extracts the longitude of the International Space Station (ISS) from the JSON response received from the API.
 
-print(response.status_code) # This will print the status code of the HTTP response, which indicates the result of the request.
+iss_position = (latitude, longitude) # This line creates a tuple called iss_position that contains the latitude and longitude of the ISS.
 
-if response.status_code != 200:
-    raise Exception("Error: API request unsuccessful") # This line raises an exception if the status code is not 200, indicating that the API request was unsuccessful.
-
-response.raise_for_status() # This line will raise an HTTPError if the HTTP request returned an unsuccessful status code (i.e., not in the 200-299 range).
+print(iss_position) # This will print the tuple containing the latitude and longitude of the International Space Station (ISS).
