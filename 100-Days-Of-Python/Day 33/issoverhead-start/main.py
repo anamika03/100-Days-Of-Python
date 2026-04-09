d318ee2e0315e06b2747c9f59b8bfd1f79c96d6d
anamika03 <anamikasingh361996@gmail.com>
2026-04-09 12:30:28 +0200

    new program

--- 100-Days-Of-Python/Day 33/issoverhead-start/main.py
+++ 100-Days-Of-Python/Day 33/issoverhead-start/main.py
@@ -1,37 +1,67 @@
+import smtplib
 import requests
 from datetime import datetime
+import time
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 49.934530
+MY_LNG = 11.583753
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
+MY_EMAIL = "singhanamika5647@gmail.com" 
+MY_PASSWORD = "owpywxwwvwqugzyf"
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-time_now = datetime.now()
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LNG - 5 <= iss_longitude <= MY_LNG + 5:
+        print("The ISS is close to you.")
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    print(time_now)
+
+    if time_now >= sunset or time_now <= sunrise:
+        print("It is currently dark.")
+
 
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        #send email
+        connection = smtplib.SMTP("smtp.gmail.com", 587)
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL, 
+            to_addrs=MY_EMAIL, 
+            msg="Subject:Look Up\n\nThe ISS is above you in the sky."
+        )
 
 
 
