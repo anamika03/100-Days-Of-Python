6e6bdcaf5c28ea5b1961a9151b37be554f528a12
anamika03 <anamikasingh361996@gmail.com>
2026-04-10 23:50:16 +0200

    UI of the program created

--- 100-Days-Of-Python/Day 34/quizzler-app-start/ui.py
+++ 100-Days-Of-Python/Day 34/quizzler-app-start/ui.py
@@ -1,2 +1,15 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.quiz = quiz_brain
+        
+
+        self.window.mainloop()
