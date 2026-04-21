f0347a58df5fe25e1ed7e98a09505187f21016df
anamika03 <anamikasingh361996@gmail.com>
2026-04-21 22:21:30 +0200

    edited

--- 100-Days-Of-Python/Web Development/Day 45/bs4-start/main.py
+++ 100-Days-Of-Python/Web Development/Day 45/bs4-start/main.py
@@ -2,12 +2,13 @@
 # It works with your favorite parser to provide idiomatic ways of navigating, searching and modifying the parse tree. 
 # Beautiful Soup is often used for web scraping, which is the process of extracting data from websites.
 
-from bs4 import BeautifulSoap
+from bs4 import BeautifulSoup
+# import lxml
 
 with open('website.html') as file:
     contents = file.read()
 
-
-
+soup = BeautifulSoup(contents, 'html.parser')
+print(soup.title.string)
 
 
