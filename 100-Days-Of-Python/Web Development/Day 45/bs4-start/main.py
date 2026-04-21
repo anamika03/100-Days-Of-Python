2bc6e3cda6bbe22fcc61c42ee601d527704575ab
anamika03 <anamikasingh361996@gmail.com>
2026-04-21 22:12:08 +0200

    new project started

---
+++ 100-Days-Of-Python/Web Development/Day 45/bs4-start/main.py
@@ -0,0 +1,13 @@
+# Beautiful Soup is a Python library for pulling data out of HTML and XML files.
+# It works with your favorite parser to provide idiomatic ways of navigating, searching and modifying the parse tree. 
+# Beautiful Soup is often used for web scraping, which is the process of extracting data from websites.
+
+from bs4 import BeautifulSoap
+
+with open('website.html') as file:
+    contents = file.read()
+
+
+
+
+
