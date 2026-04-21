84b1d2523a67c30da7bfe226474512d8fa98daa5
anamika03 <anamikasingh361996@gmail.com>
2026-04-21 22:45:36 +0200

    code written

--- 100-Days-Of-Python/Web Development/Day 45/bs4-start/main.py
+++ 100-Days-Of-Python/Web Development/Day 45/bs4-start/main.py
@@ -12,12 +12,22 @@ soup = BeautifulSoup(contents, 'html.parser')
 print(soup.title)   # <title>Anamika's Personal Site</title>
 print(soup.title.name)  # title
 print(soup.title.string)    # Anamika's Personal Site
-print(soup.title.getText())  # Anamika's Personal Site
-print(soup.title.text)  # Anamika's Personal Site   
+# print(soup.title.getText())  # Anamika's Personal Site
+# print(soup.title.text)  # Anamika's Personal Site   
 
 all_anchor_tags = soup.find_all(name="a")
 for tag in all_anchor_tags:
     print(tag.getText())    # LinkedIn
     print(tag.get("href"))  # https://www.linkedin.com/in/anamika-srivastava-9b1a5b1b3/
 
-print("\n", all_anchor_tags)
+# print("\n", all_anchor_tags)
+
+
+name = soup.find(name="h1", id="name")
+print(name)
+
+heading = soup.find(name="h3", class_="heading")
+print(heading)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
