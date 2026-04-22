69544ddc97d840d9a6ae2a32989a0f9ff43c46a8
anamika03 <anamikasingh361996@gmail.com>
2026-04-22 23:23:35 +0200

    fetched the songs name from the URL to new file

--- 100-Days-Of-Python/Web Development/Day 46/main.py
+++ 100-Days-Of-Python/Web Development/Day 46/main.py
@@ -1,3 +1,22 @@
-user_input = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-year, month, day = user_input.split("-")
+import requests
+from bs4 import BeautifulSoup
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+year, month, day = date.split("-")
 print(f"You want to travel to the year {year}, month {month} and day {day}.")
+
+
+
+URL = f"https://appbrewery.github.io/bakeboard-hot-100/{year}-{month}-{day}/"
+
+response = requests.get(url=URL)
+soup = BeautifulSoup(response.text, "html.parser")
+
+song_names_spans = soup.select("h3", class_="chart-entry__title")
+
+song_names = [song.getText().strip() for song in song_names_spans]
+
+with open("song_files.txt", "w") as file:
+    for song in song_names:
+        file.write(f"{song}\n")
+
