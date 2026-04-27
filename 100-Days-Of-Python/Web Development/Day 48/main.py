84b73e1d12c0536b08dca83aded5204651123160
anamika03 <anamikasingh361996@gmail.com>
2026-04-27 23:32:51 +0200

    new day started

---
+++ 100-Days-Of-Python/Web Development/Day 48/main.py
@@ -0,0 +1,5 @@
+# Selenium automates browsers. That's it! 
+# What you do with that power is entirely up to you. 
+# Primarily, it is for automating web applications for testing purposes, 
+# but is certainly not limited to just that. 
+# Boring web-based administration tasks can also be automated as well.
