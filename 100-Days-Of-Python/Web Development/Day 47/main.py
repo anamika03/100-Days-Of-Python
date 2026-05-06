41c915934e047b67e44e3ff941d4bc9163da01a1
anamika03 <anamikasingh361996@gmail.com>
2026-05-06 22:48:46 +0200

    email feature added mow

--- 100-Days-Of-Python/Web Development/Day 47/main.py
+++ 100-Days-Of-Python/Web Development/Day 47/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import smtplib
 
 practice_url = "https://appbrewery.github.io/instant_pot/"
 live_url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
@@ -12,5 +13,27 @@ price = soup.find(class_="a-price-whole").get_text()
 
 # price_without_currency = price.split("$")[1]
 
-price_as_float = float(price)
+price_as_float = float(price.replace(",", "").replace("$", ""))
 print(price_as_float)
+
+# =============== Send an Email ===============
+
+title = soup.find(id="productTitle").get_text()
+print(title)
+
+MY_EMAIL = "singhanamika5647@gmail.com" 
+MY_PASSWORD = "owpywxwwvwqugzyf"
+
+BUY_PRICE = 100.00
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now on sale for {price}!"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{practice_url}".encode("utf-8")
+        )
