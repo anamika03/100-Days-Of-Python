6bba7e898f12d95073ec20cd1bf0b4dd24e678da
anamika03 <anamikasingh361996@gmail.com>
2026-05-07 00:15:17 +0200

    fetch the web details using selenium

--- 100-Days-Of-Python/Web Development/Day 48/main.py
+++ 100-Days-Of-Python/Web Development/Day 48/main.py
@@ -5,13 +5,20 @@
 # Boring web-based administration tasks can also be automated as well.
 
 from selenium import webdriver # Importing the webdriver module from the selenium package
+from selenium.webdriver.common.by import By
+
+
 
 # Keep the browser open after the script ends
-chrome_options = webdriver.ChromeOptions() # Creating an instance of ChromeOptions to set options for the Chrome driver
-chrome_options.add_experimental_option("detach", True) # Adding the detach argument to keep the browser open after the script ends
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True) 
 
 driver = webdriver.Chrome(options=chrome_options) # Creating a new instance of the Chrome driver
-driver.get("https://www.amazon.de") # Navigating to the Google homepage
+driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1") # Navigating to the Google homepage
 
+price_euro = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_penny = driver.find_element(By.CLASS_NAME, value="a-price-fraction") 
+
+print(f"The price of the item is: €{price_euro.text}.{price_penny.text}") # Printing the price of the item
 # driver.close() # Closes the current tab.
-# driver.quit() # Quits the entire browser
+driver.quit() # Quits the entire browser
