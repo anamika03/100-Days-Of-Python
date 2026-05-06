27771b18f69e3c4c750799e1dcb62eaca3e44605
anamika03 <anamikasingh361996@gmail.com>
2026-05-06 23:05:52 +0200

    now usinging the .env file to secure my secrets

--- 100-Days-Of-Python/Web Development/Day 47/main.py
+++ 100-Days-Of-Python/Web Development/Day 47/main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 practice_url = "https://appbrewery.github.io/instant_pot/"
 live_url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
@@ -29,11 +33,16 @@ BUY_PRICE = 100.00
 if price_as_float < BUY_PRICE:
     message = f"{title} is now on sale for {price}!"
 
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    # ============== Use Environment Variables ===============
+
+    with smtplib.SMTP(os.environ["SMTP_ADDRESS"], port=587) as connection:
         connection.starttls()
-        result = connection.login(MY_EMAIL, MY_PASSWORD)
+        result = connection.login(os.environ["MY_EMAIL"], os.environ["MY_PASSWORD"])
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=MY_EMAIL,
+            from_addr=os.environ["MY_EMAIL"],
+            to_addrs=os.environ["MY_EMAIL"],
             msg=f"Subject:Amazon Price Alert!\n\n{message}\n{practice_url}".encode("utf-8")
         )
+
+
+
