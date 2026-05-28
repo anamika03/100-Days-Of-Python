3aedf9380f610e7c144199114f248d08008dfd46
anamika03 <anamikasingh361996@gmail.com>
2026-05-28 23:17:55 +0200

    project completed

--- 100-Days-Of-Python/Web Development/day 52/main.py
+++ 100-Days-Of-Python/Web Development/day 52/main.py
@@ -65,3 +65,21 @@ class InstaFollower:
             self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
             time.sleep(2)
     
+    
+    def follow(self):
+        # Check and update the (CSS) Selector for the "Follow" buttons as required.
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, value='._aano button')
+
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(1.1)
+            # Clicking button for someone who is already being followed will trigger dialog to Unfollow/Cancel
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(by=By.XPATH, value="//button[contains(text(), 'Cancel')]")
+                cancel_button.click()
+
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
+bot.follow()
