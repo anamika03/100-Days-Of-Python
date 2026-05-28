ef96c4356cf05186758ac34448eac822a1a0f053
anamika03 <anamikasingh361996@gmail.com>
2026-05-28 23:14:57 +0200

    new method imported

--- 100-Days-Of-Python/Web Development/day 52/main.py
+++ 100-Days-Of-Python/Web Development/day 52/main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
