260c5c84f8d07bee785734acec06e117bbb2a985
anamika03 <anamikasingh361996@gmail.com>
2026-05-28 23:17:03 +0200

    node edited

--- 100-Days-Of-Python/Web Development/day 52/main.py
+++ 100-Days-Of-Python/Web Development/day 52/main.py
@@ -4,35 +4,64 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-def login(self):
-        url = "https://www.instagram.com/accounts/login/"
-        self.driver.get(url)
-        time.sleep(4.2)
 
-        # Check if the cookie warning is present on the page
-        decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
-        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
-        if cookie_warning:
-            # Dismiss the cookie warning by clicking an element or button
-            cookie_warning[0].click()
+SIMILAR_ACCOUNT = "buzzfeedtasty" # Change this to an account of your choice
+USERNAME = "YOUR_USERNAME"
+PASSWORD = "YOUR_PASSWORD"
 
-        username = self.driver.find_element(by=By.NAME, value="username")
-        password = self.driver.find_element(by=By.NAME, value="password")
+class InstaFollower:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-        username.send_keys(USERNAME)
-        password.send_keys(PASSWORD)
+    def login(self):
+            url = "https://www.instagram.com/accounts/login/"
+            self.driver.get(url)
+            time.sleep(4.2)
 
-        time.sleep(2.1)
-        password.send_keys(Keys.ENTER)
+            # Check if the cookie warning is present on the page
+            decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
+            cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
+            if cookie_warning:
+                # Dismiss the cookie warning by clicking an element or button
+                cookie_warning[0].click()
 
-        time.sleep(4.3)
-        # Click "Not now" and ignore Save-login info prompt
-        save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
-        if save_login_prompt:
-            save_login_prompt.click()
+            username = self.driver.find_element(by=By.NAME, value="username")
+            password = self.driver.find_element(by=By.NAME, value="password")
 
-        time.sleep(3.7)
-        # Click "not now" on notifications prompt
-        notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
-        if notifications_prompt:
-            notifications_prompt.click()
+            username.send_keys(USERNAME)
+            password.send_keys(PASSWORD)
+
+            time.sleep(2.1)
+            password.send_keys(Keys.ENTER)
+
+            time.sleep(4.3)
+            # Click "Not now" and ignore Save-login info prompt
+            save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+            if save_login_prompt:
+                save_login_prompt.click()
+
+            time.sleep(3.7)
+            # Click "not now" on notifications prompt
+            notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
+            if notifications_prompt:
+                notifications_prompt.click()
+
+    def find_followers(self):
+         time.sleep(5)
+        # Will bring up the followers of an account
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/followers")
+
+        time.sleep(8.2)
+        # The xpath of the modal will change over time. Update yours accordingly.
+        modal_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+        modal = self.driver.find_element(by=By.XPATH, value=modal_xpath)
+        for i in range(5):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as an HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
+    
