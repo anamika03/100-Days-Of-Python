be34cab52a30a8b7e0f8c20cea2411d7e42c1d86
anamika03 <anamikasingh361996@gmail.com>
2026-05-28 23:13:24 +0200

    login to instagram

--- 100-Days-Of-Python/Web Development/day 52/main.py
+++ 100-Days-Of-Python/Web Development/day 52/main.py
@@ -1,30 +1,37 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
+def login(self):
+        url = "https://www.instagram.com/accounts/login/"
+        self.driver.get(url)
+        time.sleep(4.2)
 
-SIMILAR_ACCOUNT = INSTAGRAM ACCOUNT YOU WANT TO BECOME
-USERNAME = YOUR INSTAGRAM EMAIL
-PASSWORD = YOUR INSTAGRAM PASSWORD
+        # Check if the cookie warning is present on the page
+        decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
+        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
+        if cookie_warning:
+            # Dismiss the cookie warning by clicking an element or button
+            cookie_warning[0].click()
 
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
 
-class InstaFollower:
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
 
-    def __init__(self):
-        # Optional - Keep browser open (helps diagnose issues during a crash)
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
-        self.driver = webdriver.Chrome(options=chrome_options)
+        time.sleep(2.1)
+        password.send_keys(Keys.ENTER)
 
-    def login(self):
-        pass
+        time.sleep(4.3)
+        # Click "Not now" and ignore Save-login info prompt
+        save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+        if save_login_prompt:
+            save_login_prompt.click()
 
-    def find_followers(self):
-        pass
-
-    def follow(self):
-        pass
-
-
-bot = InstaFollower()
-bot.login()
-bot.find_followers()
-bot.follow()
+        time.sleep(3.7)
+        # Click "not now" on notifications prompt
+        notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
+        if notifications_prompt:
+            notifications_prompt.click()
