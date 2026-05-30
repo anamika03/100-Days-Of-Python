6463dffdf3a9b104a983012521b0cac319c1b3f0
anamika03 <anamikasingh361996@gmail.com>
2026-05-31 00:56:32 +0200

    understand the python decorator functions and the @ syntax

--- 100-Days-Of-Python/Web Development/Day 54/main.py
+++ 100-Days-Of-Python/Web Development/Day 54/main.py
@@ -35,14 +35,41 @@
 # outer_function()
 
 # # Functions can also return from other functions
-def outer_function():
+# def outer_function():
 
-    print("This is the outer function")
+#     print("This is the outer function")
 
-    def nested_function():
-        print("This is the nested function")
+#     def nested_function():
+#         print("This is the nested function")
 
-    return nested_function   # <-- no ()
+#     return nested_function   # <-- no ()
 
-inner_function = outer_function()
-inner_function()
+# inner_function = outer_function()
+# inner_function()
+
+# Python Decorator Function
+import time
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        # Do something before
+        function()
+        # Do something after
+        
+    return wrapper_function
+
+@delay_decorator
+def say_hello():
+    print("Hello!")
+
+def say_greeting():
+    print("Greetings!")
+
+@delay_decorator
+def say_goodbye():
+    print("Goodbye!")
+
+say_hello()
+say_greeting()
+say_goodbye()
