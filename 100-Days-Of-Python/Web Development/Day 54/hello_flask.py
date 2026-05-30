53fc45a2ef48c85fa59de8dd2d6874214b603964
anamika03 <anamikasingh361996@gmail.com>
2026-05-31 01:01:15 +0200

    main code edited

--- 100-Days-Of-Python/Web Development/Day 54/hello_flask.py
+++ 100-Days-Of-Python/Web Development/Day 54/hello_flask.py
@@ -6,5 +6,9 @@ app = Flask(__name__)   # __name__ is a special attribute in Python that represe
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/bye')   # This decorator tells Flask that the function below it should be called when the "/bye" URL is accessed.
+def say_bye():
+    return 'Bye!'
+
 if __name__ == '__main__':
     app.run()
