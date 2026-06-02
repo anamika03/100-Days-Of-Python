daa4d40e0515ba8c506e140364ea3c0703b81787
anamika03 <anamikasingh361996@gmail.com>
2026-06-02 09:56:46 +0200

    styling can be done in return section

--- 100-Days-Of-Python/Web Development/Day 55/main.py
+++ 100-Days-Of-Python/Web Development/Day 55/main.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<p>This is a simple Flask application.</p>' \
+           '<img src="https://www.python.org/static/community_logos/python-logo.png" alt="Python Logo">'
+
 
 @app.route('/<name>/<int:age>')
 def greet(name, age):
