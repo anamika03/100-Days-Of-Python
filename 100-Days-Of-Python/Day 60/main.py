1cb535389c29e955c5db650074a25821250313ec
anamika03 <anamikasingh361996@gmail.com>
2026-06-18 16:33:14 +0200

    day 60 completed

--- 100-Days-Of-Python/Day 60/main.py
+++ 100-Days-Of-Python/Day 60/main.py
@@ -1,33 +1,15 @@
 from flask import Flask, render_template, request
+import smtplib
 import requests
 
+posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+OWN_EMAIL = YOUR OWN EMAIL ADDRESS
+OWN_PASSWORD = YOUR EMAIL ADDRESS PASSWORD
 app = Flask(__name__)
-
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-
 
 @app.route('/')
 def get_all_posts():
     return render_template("index.html", all_posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-# SOLUTION to Challenge:
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
-        return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/post/<int:index>")
@@ -37,6 +19,29 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
